4240360b4ded6783467474820be765b0dd62d8c8
Seth T <seth10@users.noreply.github.com>
2019-01-20 18:48:13 -0500

    Add build files for GitHub Pages
    Using a postbuild script to move the build folder (which is .gitignore'd) to docs (which is what GitHub Pages looks for)

---
+++ docs/precache-manifest.ecb5053a04fae885d644f44d1dd065c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef91438fab6e98515d4c",
+    "url": "/static/css/main.5da5a7bf.chunk.css"
+  },
+  {
+    "revision": "ef91438fab6e98515d4c",
+    "url": "/static/js/main.ef91438f.chunk.js"
+  },
+  {
+    "revision": "162f273533bf3016d930",
+    "url": "/static/css/1.fe78f865.chunk.css"
+  },
+  {
+    "revision": "162f273533bf3016d930",
+    "url": "/static/js/1.162f2735.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c35675688f40ccb5400c15b9a45744c",
+    "url": "/static/media/blurredArt.3c356756.jpg"
+  },
+  {
+    "revision": "0e73966788c600c173fa8be616da9db3",
+    "url": "/index.html"
+  }
+];
